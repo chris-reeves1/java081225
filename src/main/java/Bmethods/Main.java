e06c8e44f9cc8d83d87de5e0fd546a101a91826c
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2025-12-10 09:30:31 +0000

    Update Main.java

--- src/main/java/Bmethods/Main.java
+++ src/main/java/Bmethods/Main.java
@@ -5,3 +5,187 @@ public class Main {
         
     }
 }
+
+
+package intro;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // boolean is_admin = true;
+        // boolean is_verified = false;
+        // boolean on_holiday = true;
+     
+        // if (is_admin || is_verified && !on_holiday){
+        //     System.out.println("access granted");
+        // } else {System.out.println("no access");}
+
+
+        // switches flow like if else if else
+        // multiple blocks of code can be run - hidden behind a conditional.
+        // limit to specific data types: bytes, chars, ints, shorts, strings.
+        // default (like else) - optional, can go anywhere. 
+        // Fall through logic.
+        
+        //  - Backward compability
+        //  - avoid repitiion
+        //  - efficiancy/flex (os)
+        //  - Comment fall through if intended. 
+
+        // int day = 1;
+
+        // switch (day) {
+        //     case 1: 
+        //         System.out.println("monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("tuesday");
+        //         break;
+        //     default:
+        //         System.out.println("other");
+        // }
+
+        // String grade = "a";
+
+        // switch (grade) {
+        //     case "a", "b", "c" -> System.out.println("passed");
+        //     case "d", "f" -> System.out.println("failed");
+        //     default -> System.out.println("na");
+        // }
+
+        // loops + arrays
+
+        //while, do-while, for, enhanced for.
+
+        // while - needs a condition to be met to start + a condition to stop
+        
+        // int i = 0; // counter
+
+        // while(i < 100){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // do-while - always runs at least once. 
+
+        // int i = 5;
+
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 5);
+
+        // Scanner sc = new Scanner(System.in);
+        // int choice;
+
+        // do {
+        //     System.out.println("1. addition");
+        //     System.out.println("2. Subtraction");
+        //     System.out.println("4. exit");
+        //     choice = sc.nextInt();
+
+        //     switch (choice){
+        //         case 1:
+        //             System.out.println("adding.....");
+        //             break;
+        //         case 2:
+        //             System.out.println("subtracting.....");
+        //             break;
+        //         case 4:
+        //             System.out.println("exiting....");
+        //             break;
+        //         default:
+        //             System.out.println("invalid choice!!!");
+        //     }
+        // } while(choice != 4);
+
+        // basic for and enhanced for
+        // (initialisation ; condition ; increment)
+    //     for(int x = 1; x < 5; x++){
+    //         System.out.println(x);
+    //     }
+
+    //     // enhanced for loop loops through an iterable:
+    //     int[] numbers = {1, 2, 3, 4};
+
+    //     for (int number: numbers){
+    //         System.out.println(number);
+    
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    Arrays:
+        - collection: of values
+        - must be of the same type.
+        
+    syntax:
+        - //declaring
+        - int[] arrayInt;
+        - type arrayType[] // also valid
+
+        declare and initialise:
+        - int[] arrayint = new int[5] //sets the length
+        - int[] arrayint = {1, 2, 3, 4} // literal initialistion.
+
+        characteristics:
+            - fixed length
+            - zero-based indexing
+            - Only one data type
+            - NO inbuilt methods
+
+        in-memory:
+            - contiguous memory in the heap. 
+            - primitives in heap. 
+            - referneces stored in the arrays memory boxes(not the actual object)
+
+        defaults:
+            int: 0;
+            boolean: False
+            float: 0.0
+            obj/string: null
+            char: u/0000
+
+        why use: 
+            - speed + predictable performance and code
+            - memory efficiant
+            - legacy code
+            - best for primitives.
+            - outofboundserror
+    */
+
+            int[] numbers = {10, 20, 30};
+
+            numbers[0] = 100;
+
+            System.out.println(numbers.length);
+
+            // copy + copyof + nested. 
+
+
+
+
+
+
+
+
+}
+}
